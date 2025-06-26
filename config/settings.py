e064ef014adb4817c27de9fc000250d8dd231e8c
Bramwel M.M <mutugibramwel@gmail.com>
2025-06-27 02:12:37 +0300

    customer registration and login

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = '/'  # or 'dashboard'
+LOGOUT_REDIRECT_URL = 'login'
+
