19ab2c9e811c9f8ee19fcf768e4afab87b286f63
KXvira <kevinkimurgor@kabark.com>
2025-07-11 11:11:07 +0300

    Add API module for user registration with axios

---
+++ vite-frontend/api/index.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+const API = axios.create({baseURL: "http://127.0.0.1:8000/"})
+
+//API calls
+
+//Register
+export const userRegister = (formData) => {
+    return API.post("/api/accounts/register/", formData)
+}
