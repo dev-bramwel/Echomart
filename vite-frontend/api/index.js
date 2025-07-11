061e4ef93fd12c4a57a85022778c51665ad6558b
Kevin <161073329+KXvira@users.noreply.github.com>
2025-07-11 16:58:58 +0300

    Kevin frontend (#23)
    * Add success message handling to user registration form
    
    * Refactor user authentication API functions for clarity and consistency
    
    * Enhance login form with validation and error handling
    
    ---------
    
    Co-authored-by: KXvira <kevinkimurgor@kabark.com>

--- vite-frontend/api/index.js
+++ vite-frontend/api/index.js
@@ -8,3 +8,8 @@ const API = axios.create({baseURL: "http://127.0.0.1:8000/"})
 export const userRegister = (formData) => {
     return API.post("/api/accounts/register/", formData)
 }
+
+//Login
+export const userLogin = (formData) => {
+    return API.post("api/accounts/login/", formData)
+}
