cf9a4661cdc7c4de4b685c9f7deab286e218e305
KXvira <kevinkimurgor@kabark.com>
2025-07-11 18:36:55 +0300

    Add token handling in API requests using js-cookie

--- vite-frontend/api/index.js
+++ vite-frontend/api/index.js
@@ -1,6 +1,15 @@
 import axios from "axios"
+import Cookies from "js-cookie"; 
 
 const API = axios.create({baseURL: "http://127.0.0.1:8000/"})
+
+
+// Attach token from cookies
+API.interceptors.request.use((req) => {
+  const token = Cookies.get("token");
+  if (token) req.headers.Authorization = `Bearer ${token}`;
+  return req;
+});
 
 //API calls
 
@@ -12,4 +21,9 @@ export const userRegister = (formData) => {
 //Login
 export const userLogin = (formData) => {
     return API.post("api/accounts/login/", formData)
+}
+
+//Logout
+export const logout = () => {
+    return API.post("/api/accounts/logout/")
 }
