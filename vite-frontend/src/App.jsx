56ea13f3e5c7c406d4c47e92bf3afe97a3f3676e
3ddie0chieng <edwinochieng678@gmail.com>
2025-07-18 03:18:06 +0300

    Eddie Frontend

--- vite-frontend/src/App.jsx
+++ vite-frontend/src/App.jsx
@@ -2,7 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Categorybar from "./Components/Categorybar/Categorybar.jsx";
 import Login from "./Components/LoginSignup/Login.jsx";
+
 import Signup from "./Components/LoginSignup/Signup.jsx";
+import PasswordRecovery from "./Components/LoginSignup/PasswordRecovery.jsx";
+import Passcode from "./Components/LoginSignup/Passcode.jsx";
 
 const App = () => {
   return (
@@ -12,6 +15,8 @@ const App = () => {
         <Route path="/" element={<Categorybar />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/password-recovery" element={<PasswordRecovery />} />
+        <Route path="/passcode" element={<Passcode />} />
       </Routes>
     </Router>
   );
